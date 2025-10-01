cb0e229a67fb5b13a892562445436428a3b62d36
Ambharapu Ajay Kumar Reddy <ajayreddy.fsd@gmail.com>
2025-10-01 10:56:47 -0400

    Time: 106 ms (41.37%) | Memory: 28.6 MB (23.69%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,19 +1,31 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        # 2 pointers, one at start and one at end
         p1 = 0
         p2 = len(height)-1
+
+        # initialize area and max_area
         area = 0
         max_area = 0
 
-
+        # while pointers don’t cross
         while(p1 < p2):
-            area = min(height[p1], height[p2])*(p2-p1)
+            # area formula = min height × distance
+            # why min? coz water only goes till smaller height, and no slanting
+            area = min(height[p1], height[p2]) * (p2 - p1)
+
+            # keep track of the biggest area so far
             max_area = max(area, max_area)
 
+            # now time to move pointers
+            # whichever has smaller height, we move that, coz min height is the deciding factor
             if height[p1] < height[p2]:
-                p1 +=1
+                p1 += 1
             elif height[p2] < height[p1]:
                 p2 -= 1
             else:
-                p1 +=1
+                # if both have equal heights, can move either
+                p1 += 1
+
+        # finally return the max_area we got
         return max_area
