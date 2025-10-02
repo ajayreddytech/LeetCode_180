7005e02415b82231f85fd48feadf6b6ee129dca0
Ambharapu Ajay Kumar Reddy <ajayreddy.fsd@gmail.com>
2025-10-01 20:47:46 -0400

    Time: 27 ms (28.83%) | Memory: 19.1 MB (67.01%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,15 +1,33 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-        for i in range(32):
-            sum = 0
-            for num in nums:
-                if(num & (1 << i)):
-                    sum += 1 #? why adding 1 and not the bit
-            if (sum % 3 == 1):
-                result |= (1 << i)
 
-         # handle negative numbers
-        if result >= 2**31:
-            result -= 2**32
+        # this is the result which we gonna return at the end
+        # currently inid as zero, but we gonna change it bit by bit to get the actual no.
+        result = 0
+
+        # 32 iterations - why 32? 
+        # coz we all know computer stores everything as binary internally
+        # but how many bits in each of those binary nums - answer is 32 
+        # also leetcode problem statement says, each num is stored in 32 bits
+        for i in range(32):
+
+            # we need this to compute the sum of all the bits at bit-pos-i
+            sum = 0
+
+            # iterating through all the nums and computing the sum of all bits at bit-pos-i in all nums
+            for num in nums:
+                sum += (num >> i) & 1
+            
+            # once sum is computed, we gonna do modulo 3, since binary, we gonna get either 0 or 1 as the result
+            # if it is zero, we need not change the bit in result at bit-pos-i, since it is already 0
+            # if it is one, we just change the bit in result at bit-pos-i to one
+            if (sum % 3 == 1):
+                result = (1 << i) | result
+
+        # handle negative numbers
+        # if the result is greater than the largest possible +ve number (given in the leetcode problem description)
+        # then just wrap around by subtracting 2**32
+        if result > 2**31 - 1:
+            result = result - 2**32
+
         return result
