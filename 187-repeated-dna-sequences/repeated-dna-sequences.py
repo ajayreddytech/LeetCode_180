534fd576bcc5b8ead504508635dfc6782eeac3cc
Ambharapu Ajay Kumar Reddy <ajayreddy.fsd@gmail.com>
2025-10-03 16:06:46 -0400

    Time: 27 ms (46.05%) | Memory: 29.4 MB (6.78%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,21 +1,31 @@
 # if no duplicates, return []
-
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        # first, check if the string length <= 10, if yes return empty array
         if len(s) <= 10: return []
 
+        # make our first sliding window (first 10 chars)
         sw = s[0:9+1]
+
+        # make our 2 sets - seen and repeated
         seen = set()
         repeated = set()
 
+        # add first window to seen
         seen.add(sw)
 
+        # loop through the string to move the sliding window
         for i in range(1, len(s)-10+1):
+            # tweak sliding window: remove first char, add next char from main string
             sw = sw[1:] + s[i+9]
+            
+            # check if current window is already in seen
             if sw in seen:
+                # if yes, add to repeated
                 repeated.add(sw)
             else:
+                # if not, add to seen
                 seen.add(sw)
         
+        # finally convert repeated set to list and return
         return list(repeated)
-
